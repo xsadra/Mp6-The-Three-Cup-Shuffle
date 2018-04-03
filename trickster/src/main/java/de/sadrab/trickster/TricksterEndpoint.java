4e6f44b921c109d6d737655ee40168fdd97b0841
Sadra <ti9mail@gmail.com>
2018-04-04 01:37:45 +0200

    Finish MP6

--- trickster/src/main/java/de/sadrab/trickster/TricksterEndpoint.java
+++ trickster/src/main/java/de/sadrab/trickster/TricksterEndpoint.java
@@ -1,4 +1,62 @@
 package de.sadrab.trickster;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Random;
+
+@RestController
 public class TricksterEndpoint {
+
+    @Value("#{'${cup.ports}'.split(',')}")
+    private List<String> cupPorts;
+
+    private RestTemplate restTemplate;
+
+    public TricksterEndpoint(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/play")
+    void play(){
+        deleteCoinFromCups();
+
+        PutCoinIntoRandomCup();
+    }
+
+    @GetMapping("/choose/{number}")
+    Boolean choose(@PathVariable String number){
+        String cupUrl = getCupUrl(number);
+
+        ResponseEntity<Boolean> response = restTemplate.getForEntity(cupUrl, Boolean.class);
+
+        return response.getBody();
+    }
+
+
+    private String getCupUrl(String number) {
+        String port = cupPorts.get(Integer.valueOf(number));
+        return "http://localhost:" + port + "/coin";
+    }
+
+    private void PutCoinIntoRandomCup() {
+        Random randomNumber=new Random(cupPorts.size());
+        String randomPort = cupPorts.get(randomNumber.nextInt());
+        String randomCupUrl = "http://localhost:" + randomPort + "/coin";
+        restTemplate.put(randomCupUrl,null);
+    }
+
+    private void deleteCoinFromCups() {
+        cupPorts.stream()
+                .map(port -> "http://localhost:" + port + "/coin")
+                .forEach(url -> restTemplate.delete(url));
+    }
+
+
 }
--- trickster/src/main/java/de/sadrab/trickster/configuration/RestConfiguration.java
+++ trickster/src/main/java/de/sadrab/trickster/configuration/RestConfiguration.java
@@ -1,4 +1,13 @@
 package de.sadrab.trickster.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class RestConfiguration {
+    @Bean
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
